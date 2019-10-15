55367fff0fe61a3bc704fa0ddbb9fe51ff6877d9
vinu-129 <vinay.chudasama@1rivet.local>
2019-10-14 21:41:28 -0700

    responsive  sidebar  and header

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-// // Navbar toggler
-// document.getElementById("nav-toggle").onclick = function(){
-//     document.getElementById("side-nav").setAttribute("style", "width:250px");
-//     document.getElementById("logo-image").setAttribute("src","./assets/images/icons/white-logo.png");
-    
-//     document.getElementById("nav-link-title").classList.remove("d-none");
-
-// }
